42c74e6d69d99c3025f269109e784a79aeb57c84
Mickael Istria <mistria@redhat.com>
2025-07-23 20:59:09 +0200

    Consider module filtering when matching type reference

--- org.eclipse.jdt.core.javac/src/org/eclipse/jdt/internal/core/search/matching/DOMTypeDeclarationLocator.java
+++ org.eclipse.jdt.core.javac/src/org/eclipse/jdt/internal/core/search/matching/DOMTypeDeclarationLocator.java
@@ -10,9 +10,15 @@
  *******************************************************************************/
 package org.eclipse.jdt.internal.core.search.matching;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.OperationCanceledException;
 import org.eclipse.jdt.core.compiler.CharOperation;
 import org.eclipse.jdt.core.dom.AbstractTypeDeclaration;
 import org.eclipse.jdt.core.dom.AnnotationTypeDeclaration;
@@ -22,6 +28,16 @@ import org.eclipse.jdt.core.dom.IModuleBinding;
 import org.eclipse.jdt.core.dom.ITypeBinding;
 import org.eclipse.jdt.core.dom.LambdaExpression;
 import org.eclipse.jdt.core.dom.TypeDeclaration;
+import org.eclipse.jdt.core.search.IJavaSearchConstants;
+import org.eclipse.jdt.core.search.IJavaSearchScope;
+import org.eclipse.jdt.core.search.SearchDocument;
+import org.eclipse.jdt.core.search.SearchEngine;
+import org.eclipse.jdt.core.search.SearchMatch;
+import org.eclipse.jdt.core.search.SearchParticipant;
+import org.eclipse.jdt.core.search.SearchPattern;
+import org.eclipse.jdt.core.search.SearchRequestor;
+import org.eclipse.jdt.internal.core.JavaModelManager;
+import org.eclipse.jdt.internal.core.search.JavaSearchParticipant;
 import org.eclipse.jdt.internal.core.search.LocatorResponse;
 import org.eclipse.jdt.internal.core.search.indexing.IIndexConstants;
 
@@ -81,9 +97,7 @@ public class DOMTypeDeclarationLocator extends DOMPatternLocator {
 	@Override
 	public LocatorResponse resolveLevel(org.eclipse.jdt.core.dom.ASTNode node, IBinding binding, MatchLocator locator) {
 		if (binding == null) return toResponse(INACCURATE_MATCH);
-		if (!(binding instanceof ITypeBinding)) return toResponse(IMPOSSIBLE_MATCH);
-
-		ITypeBinding type = (ITypeBinding) binding;
+		if (!(binding instanceof ITypeBinding type)) return toResponse(IMPOSSIBLE_MATCH);
 
 		if (!matchSearchForTypeSuffix(type, this.locator.pattern.typeSuffix)) {
 			return toResponse(IMPOSSIBLE_MATCH);
@@ -122,7 +136,7 @@ public class DOMTypeDeclarationLocator extends DOMPatternLocator {
 		String bindModName = module.getName();
 
 		if (typePattern.modulePatterns == null) {// use 'normal' matching
-			char[][] moduleList = this.locator.getModuleList(typePattern);
+			char[][] moduleList = getModuleList(typePattern);
 			for (char[] m : moduleList) { // match any in the list
 				int ret = this.locator.matchNameValue(m, bindModName.toCharArray());
 				if (ret != IMPOSSIBLE_MATCH) return ret;
@@ -136,4 +150,76 @@ public class DOMTypeDeclarationLocator extends DOMPatternLocator {
 		return IMPOSSIBLE_MATCH;
 	}
 
+	private HashSet<String> getModuleGraph(String mName, TypeDeclarationPattern typePattern, HashSet<String> mGraph) {
+		mGraph.add(mName);
+		SearchPattern modulePattern = SearchPattern.createPattern(mName,
+				IJavaSearchConstants.MODULE, IJavaSearchConstants.DECLARATIONS, typePattern.getMatchRule());
+		if (modulePattern == null) return mGraph;
+		final HashSet<String> tmpGraph = new HashSet<>();
+		final SearchParticipant participant = new JavaSearchParticipant() {
+			@Override
+			public void locateMatches(SearchDocument[] indexMatches, SearchPattern mPattern,
+					IJavaSearchScope scope, SearchRequestor requestor, IProgressMonitor monitor) throws CoreException {
+				DOMMatchLocator matchLocator =	new DOMMatchLocator(mPattern,	requestor,	scope,	monitor);
+				/* eliminating false matches and locating them */
+				if (monitor != null && monitor.isCanceled()) throw new OperationCanceledException();
+				matchLocator.locateMatches(indexMatches);
+				addRequiredModules(matchLocator);
+			}
+			private void addRequiredModules(DOMMatchLocator matchLocator) {
+				for (IModuleBinding m :matchLocator.moduleBindings) {
+					if (m.getName() != null && !m.getName().isEmpty()) {
+						tmpGraph.add(new String(m.getName()));
+						for (IModuleBinding r : m.getRequiredModules()) {
+							char[] name = r.getName().toCharArray();
+							if (name == null || CharOperation.equals(name, CharOperation.NO_CHAR)) continue;
+							tmpGraph.add(new String(name));
+						}
+					}
+				}
+			}
+		};
+		final SearchRequestor requestor = new SearchRequestor() {
+			@Override
+			public void acceptSearchMatch(SearchMatch searchMatch) throws CoreException {
+				// do nothing
+				if (JavaModelManager.VERBOSE) {
+					JavaModelManager.trace(searchMatch.toString());
+				}
+			}
+		};
+		try {
+			new SearchEngine().search(modulePattern, new SearchParticipant[] {participant},
+					JavaModelManager.getJavaModelManager().getWorkspaceScope(),
+					requestor,	null);
+		} catch (CoreException e) {
+			// do nothing
+		}
+		mGraph.addAll(tmpGraph);
+		return mGraph;
+	}
+	protected char[][] getModuleList(TypeDeclarationPattern typePattern) {
+		if (!typePattern.moduleGraph)
+			return typePattern.moduleNames;
+		if (typePattern.moduleGraphElements != null) // already computed
+			return typePattern.moduleGraphElements;
+		typePattern.moduleGraphElements = CharOperation.NO_CHAR_CHAR; // signal processing done.
+		// compute (lazy)
+		List<String> moduleList = Arrays.asList(CharOperation.toStrings(typePattern.moduleNames));
+		int sz = moduleList.size();
+		HashSet<String> mGraph = new HashSet<>();
+		for (int i = 0; i < sz; ++i) {
+			mGraph = getModuleGraph(moduleList.get(i), typePattern, mGraph);
+		}
+		sz = mGraph.size();
+		if (sz > 0) {
+			String[] ar = mGraph.toArray(new String[0]);
+			char[][] tmp = new char[sz][];
+			for (int i = 0; i < sz; ++i) {
+				tmp[i] = ar[i].toCharArray();
+			}
+			typePattern.moduleGraphElements = tmp;
+		}
+		return typePattern.moduleGraphElements;
+	}
 }
