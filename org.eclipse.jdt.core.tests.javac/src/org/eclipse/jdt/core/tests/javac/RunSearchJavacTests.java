7e3088fac896656e732f4bdb38bcc4d207eaa5a2
Mickael Istria <mistria@redhat.com>
2025-08-26 15:07:43 +0200

    Don't delete JavaSearch and JavaSearch15 test projects

--- org.eclipse.jdt.core.tests.javac/src/org/eclipse/jdt/core/tests/javac/RunSearchJavacTests.java
+++ org.eclipse.jdt.core.tests.javac/src/org/eclipse/jdt/core/tests/javac/RunSearchJavacTests.java
@@ -10,12 +10,25 @@
  *******************************************************************************/
 package org.eclipse.jdt.core.tests.javac;
 
+import org.eclipse.jdt.core.tests.model.AbstractJavaSearchTests;
+import org.eclipse.jdt.core.tests.model.JavaSearchBugsTests;
+import org.eclipse.jdt.core.tests.model.JavaSearchBugsTests2;
 import org.eclipse.jdt.core.tests.model.RunJavaSearchTests;
+
+import junit.framework.Test;
 
 public class RunSearchJavacTests extends RunJavaSearchTests {
 
 	public RunSearchJavacTests(String name) {
 		super(name);
+	}
+
+	public static Test suite() {
+		Test res = RunJavaSearchTests.suite();
+		// Adds the 2 other suites that requires JavaSearch and JavaSearch15 test projects to run well
+		AbstractJavaSearchTests.JAVA_SEARCH_SUITES.add(JavaSearchBugsTests.class);
+		AbstractJavaSearchTests.JAVA_SEARCH_SUITES.add(JavaSearchBugsTests2.class);
+		return res;
 	}
 
 }
