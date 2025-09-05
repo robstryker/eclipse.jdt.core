acaeb9eb43dedd304e8cb5dc8cd20d2d867db21e
Mickael Istria <mistria@redhat.com>
2025-09-05 16:26:28 +0200

    Remove useless imports in tests

--- org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/dom/ASTConverter15JLS8Test.java
+++ org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/dom/ASTConverter15JLS8Test.java
@@ -7014,8 +7014,6 @@ public class ASTConverter15JLS8Test extends ConverterTestSetup {
 	public void test0218() throws JavaModelException {
     	this.workingCopy = getWorkingCopy("/Converter15/src/X.java", true/*resolve*/);
     	String contents =
-			"import java.util.List;\n" +
-			"\n" +
 			"public class X {\n" +
 			"	/**\n" +
 			"	 * @category fo\n" +
@@ -7056,8 +7054,6 @@ public class ASTConverter15JLS8Test extends ConverterTestSetup {
 	public void test0219() throws JavaModelException {
 		this.workingCopy = getWorkingCopy("/Converter15/src/X.java", true/*resolve*/);
 		String contents =
-			"import java.util.List;\n" +
-			"\n" +
 			"public class X {\n" +
 			"	/**\n" +
 			"	 * @category fo\n" +
