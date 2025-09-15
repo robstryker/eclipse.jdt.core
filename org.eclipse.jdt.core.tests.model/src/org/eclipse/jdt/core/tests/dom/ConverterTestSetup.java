38b6503d60e1cdefd21beef324a245597aa47d3f
Mickael Istria <mistria@redhat.com>
2025-09-15 09:36:43 +0200

    Match "This instance method cannot override the static method from X" in test

--- org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/dom/ConverterTestSetup.java
+++ org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/dom/ConverterTestSetup.java
@@ -1066,6 +1066,8 @@ public abstract class ConverterTestSetup extends AbstractASTTests {
 			return (arguments[0] + " is not an annotation type").equals(expected);
 		case IProblem.UndefinedAnnotationMember:
 			return ("The attribute " + arguments[0] + " is undefined for the annotation type " + arguments[3]).replaceAll("@interface ", "").equals(expected);
+		case IProblem.CannotOverrideAStaticMethodWithAnInstanceMethod:
+			return ("This instance method cannot override the static method from " + arguments[3]).replaceAll("@interface ", "").equals(expected);
 		case IProblem.Syntax:
 			return original.equals(switch (expected) {
 				case "Syntax error, insert \"Finally\" to complete BlockStatements" -> "'try' without 'catch', 'finally' or resource declarations";
