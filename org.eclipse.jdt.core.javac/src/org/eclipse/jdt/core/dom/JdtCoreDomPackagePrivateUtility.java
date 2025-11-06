5d93d04f62d9194b7e9b9882ad32770c6df6aca9
Rob Stryker <rob@oxbeef.net>
2025-11-06 13:27:31 -0500

    Fix testBug123836j
    Signed-off-by: Rob Stryker <rob@oxbeef.net>
    
    Fix regressions
    
    Signed-off-by: Rob Stryker <rob@oxbeef.net>
    
    Fix issues with patch
    
    Signed-off-by: Rob Stryker <rob@oxbeef.net>
    
    Fix obvious error
    
    Signed-off-by: Rob Stryker <rob@oxbeef.net>
    
    Fix regression in testMethodReference14
    
    Signed-off-by: Rob Stryker <rob@oxbeef.net>

--- org.eclipse.jdt.core.javac/src/org/eclipse/jdt/core/dom/JdtCoreDomPackagePrivateUtility.java
+++ org.eclipse.jdt.core.javac/src/org/eclipse/jdt/core/dom/JdtCoreDomPackagePrivateUtility.java
@@ -1,12 +1,20 @@
 package org.eclipse.jdt.core.dom;
 
 public class JdtCoreDomPackagePrivateUtility {
+	public static BindingResolver getBindingResolver(AST ast) {
+		return ast.getBindingResolver();
+	}
+
 	public static BindingResolver getBindingResolver(ASTNode node) {
 		return node.getAST().getBindingResolver();
 	}
 
 	public static JavacBindingResolver getJavacBindingResolverOrNull(ASTNode node) {
-		BindingResolver br = getBindingResolver(node);
+		return getJavacBindingResolverOrNull(node.getAST());
+	}
+
+	public static JavacBindingResolver getJavacBindingResolverOrNull(AST ast) {
+		BindingResolver br = getBindingResolver(ast);
 		if( br != null ) {
 			if( br instanceof JavacBindingResolver br2 )
 				return br2;
@@ -15,7 +23,11 @@ public class JdtCoreDomPackagePrivateUtility {
 	}
 
 	public static IBinding findBindingForType(ASTNode node, String signature) {
-		JavacBindingResolver jcbr = getJavacBindingResolverOrNull(node);
+		return findBindingForType(node.getAST(), signature);
+	}
+
+	public static IBinding findBindingForType(AST ast, String signature) {
+		JavacBindingResolver jcbr = getJavacBindingResolverOrNull(ast);
 		IBinding ret1 = jcbr.findBinding(signature);
 		if( ret1 == null ) {
 			String sig2 = signature.replaceAll("\\.", "/");
@@ -25,7 +37,11 @@ public class JdtCoreDomPackagePrivateUtility {
 	}
 
 	public static IBinding findUnresolvedBindingForType(ASTNode node, String signature) {
-		JavacBindingResolver jcbr = getJavacBindingResolverOrNull(node);
+		return findUnresolvedBindingForType(node.getAST(), signature);
+	}
+
+	public static IBinding findUnresolvedBindingForType(AST ast, String signature) {
+		JavacBindingResolver jcbr = getJavacBindingResolverOrNull(ast);
 		IBinding ret1 = jcbr instanceof JavacBindingResolver br2 ? br2.findBinding(signature) : null;
 		if( ret1 == null ) {
 			ret1 = jcbr instanceof JavacBindingResolver br2 ? br2.findUnresolvedBinding(signature) : null;
